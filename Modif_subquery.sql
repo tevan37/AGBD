39f6ea98deba980d855d6f2f0e6f605d090b0a05
Esteban Huanca Velarde <ehuancav132005@gmail.com>
2024-06-13 12:10:59 -0300

    actividad 1/4 hecha

--- Modif_subquery.sql
+++ Modif_subquery.sql
@@ -2,3 +2,8 @@
 INSERT INTO regions(region_name)
 VALUES ("Africa"), ("Middle East");
 
+-- Modificar para pasar los paises a las nuevas regiones
+
+-- Borrar la region combinada
+
+-- Agregar dos paises mas a cada una de las nuevas regiones
